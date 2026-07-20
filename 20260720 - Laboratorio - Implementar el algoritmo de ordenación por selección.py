4b0f51eea4e9929c7c11a007e92359588de0b94f
Matias Fons <matias.fons@gmail.com>
2026-07-20 15:20:25 +0200

    automatic update fCC proyects - 2026-07-20 15:20

--- 20260720 - Laboratorio - Implementar el algoritmo de ordenación por selección.py
+++ 20260720 - Laboratorio - Implementar el algoritmo de ordenación por selección.py
@@ -5,11 +5,20 @@
 
 def selection_sort (lista:list) -> list:
 
-    for i in range(len(lista)-1): # cuando ya se acomodó hasta el anteúltimo número, el último que queda al final ya queda ordenado solo. Así que este bucle no necesita recorrer toda la lista, puede frenar uno antes.
+    for i in range(len(lista)-1): # cuando ya se acomodó hasta el anteúltimo número, el último que queda al final ya queda ordenado solo. Luego, este bucle puede frenar uno antes.
         indice_minimo=i # supongo que es el mínimo hasta que se demuestre lo contrario
         for j in range(i + 1, len(lista)): # range(inicio, fin): j arranca en la posición siguiente a i y camina hasta el final absoluto de la lista
             # Comparamos el elemento actual del recorrido (j) contra nuestro candidato a mínimo
             if lista[j]<lista[indice_minimo]: # si el elemento que estoy mirando es menor que el mínimo que tengo hasta ahora, entonces actualizo el índice del mínimo
                 indice_minimo=j
+
+        # Antes del intercambio verifico si el índice del mínimo es diferente al índice actual (i) para evitar intercambios innecesarios
+        if indice_minimo != i:    
+            # Creo un elemento auxiliar para hacer el intercambio
+            temp = lista[i]
+            lista[i] = lista[indice_minimo] # Asigno el valor del mínimo a la posición actual (i)
+            lista[indice_minimo] = temp # Asigno el valor que estaba en la posición actual (i) a la posición del mínimo (indice_minimo)
+
+
 
 # falta definir el intercambio, solo lo hacemos si el índice del mínimo es diferente al índice actual (i)
